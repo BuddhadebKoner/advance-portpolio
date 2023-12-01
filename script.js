34179ea817ea8b418aefb9e76904d2fe4b239263
Buddhadeb koner <iambuddhdebkoner@gmail.com>
2023-12-01 14:50:54 +0530

    dark mode and light mode

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-console.log("sucessfull!");
+console.log("successful!");
 
 var switchCheckbox = document.getElementById('switch');
 var headerElement = document.querySelector('header');
 var elements = document.querySelector('.elements');
+// access cover images light and dark
+var darkImg = document.getElementById('dark-img');
+var lightImg = document.getElementById('light-img');
+// for banner bg
+var bannerBg = document.querySelector('.banner');
 
+function darkLightMode() {
+    const isSwitchChecked = switchCheckbox.checked;
+    const isDarkMode = isSwitchChecked;
 
-// Add an event listener to detect changes in the checkbox state
-switchCheckbox.addEventListener('change', function() {
-  // Check if the checkbox is checked
-  if (switchCheckbox.checked) {
-    headerElement.classList.remove('light-mode');
-    headerElement.classList.add('dark-mode');
-    // elements.classList.add('elements-checked');
-  } else{
-    headerElement.classList.remove('dark-mode');
-    headerElement.classList.add('light-mode');
-    // elements.classList.remove('elements-checked');
-  }
+    headerElement.classList.toggle('light-mode', !isDarkMode);
+    headerElement.classList.toggle('dark-mode', isDarkMode);
+    elements.classList.toggle('elements-checked', isSwitchChecked);
+
+    darkImg.style.display = isDarkMode ? 'block' : 'none';
+    lightImg.style.display = isDarkMode ? 'none' : 'block';
+
+    // Toggle banner background color
+    bannerBg.style.backgroundColor = isDarkMode ? 'black' : 'white';
+}
+
+darkLightMode();
+switchCheckbox.addEventListener('change', function () {
+    darkLightMode();
 });
-
