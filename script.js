2b4197490c3ea27cd4c1bf503aa479e7c72d1cd7
Buddhadeb koner <iambuddhdebkoner@gmail.com>
2023-12-03 14:05:00 +0530

    ddn

--- script.js
+++ script.js
@@ -1,36 +1,63 @@
 console.log("successful!");
 
-var switchCheckbox = document.getElementById('switch');
-// for body bg ----------------
-var BodyElement = document.querySelector('body');
-// for heading backgrounf -------
-var headerElement = document.querySelector('header');
-// for the links and text in the header --
-var elements = document.querySelector('.elements');
-// access cover images light and dark--
-var darkImg = document.getElementById('dark-img');
-var lightImg = document.getElementById('light-img');
-// for banner bg--
-var bannerBg = document.querySelector('.banner');
 
+
+let switchCheckbox = document.getElementById('switch');
+
+
+// dark mode light mode function is here.--
 function darkLightMode() {
     const isSwitchChecked = !switchCheckbox.checked; // Change to !switchCheckbox.checked for dark mode by default
     const isDarkMode = isSwitchChecked; // when checked dark mode enable .
 
-    BodyElement.classList.toggle('light-mode', !isDarkMode); // body light mode bg
-    BodyElement.classList.toggle('dark-mode', isDarkMode); // body dark mode bg
+    
+    headerContent(isDarkMode); // for the header bg and text change with toggling 
+    imageToggle(isDarkMode); // banner image toggle change 
+    BannerLayout(isDarkMode); // this is for the bg of banner --
+}
 
-    headerElement.classList.toggle('light-mode', !isDarkMode); // header bg
-    headerElement.classList.toggle('dark-mode', isDarkMode); // header bg
 
-    elements.classList.toggle('elements-checked', isSwitchChecked); // for the text color that is var(--light-bg) , 'elements-checked' this is a class that has a var(--light-bg) bg .
+// banner image is change with toggling.
+function imageToggle(isDarkMode) {
+    // access cover images light and dark --
+    let darkImg = document.getElementById('dark-img');
+    let lightImg = document.getElementById('light-img');
 
     darkImg.style.display = isDarkMode ? 'block' : 'none'; // a coding bg image for the dark mode
     lightImg.style.display = isDarkMode ? 'none' : 'block';  // complement img for light
-
-    // Toggle banner background color
-    bannerBg.style.backgroundColor = isDarkMode ? 'var(--dark-bg)' : 'var(--light-bg)';
 }
+// header bg and text is change with toggling.
+
+function headerContent(isDarkMode) {
+    // access the elements--- 
+    const headerBgElement = document.querySelector('.heading-content');
+    const logo = document.getElementById('logo');
+    const navlinks = Array.from(document.getElementsByClassName('nav-links-tag'));
+
+    navlinks.forEach(link => link.style.color = isDarkMode ? 'white' : 'black');
+
+    headerBgElement.style.backgroundColor = isDarkMode ? 'var(--header-bg)' : 'var(--White)';
+    logo.style.color = isDarkMode ? 'var(--header-btn-light)' : 'var(--header-btn-dark)';
+}
+function BannerLayout(isDarkMode) {
+    const banner = document.querySelector('.banner');
+    banner.style.color = isDarkMode ? 'var(--header-btn-light)' : 'var(--header-btn-dark)';
+    console.log("hi");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 darkLightMode(); // first call the function for the default state -- that is dark
 
